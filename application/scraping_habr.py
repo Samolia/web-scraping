8658742f4e5422a3900c3196b22ae33075468a05
samolia <samolia9@gmail.com>
2021-05-18 21:12:08 +0300

    refactoring

--- application/scraping_habr.py
+++ application/scraping_habr.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def scraping_by_preview(url, keywords):
     article_info = []
+    keywords_pattern = '|'.join(keywords)
     response = requests.get(url)
     if not response.ok:
         raise ValueError('response is not valid')
     soup = BeautifulSoup(response.text, features='html.parser')
     for article in soup.find_all('article'):
         hubs = {hub.text for hub in article.find_all('a', class_='hub-link')}
-        if hubs & keywords:
-            date = article.find('span', class_='post__time').text
-            post_title = article.find('h2', class_='post__title').text.strip()
-            href = article.find('h2', class_='post__title').find('a').attrs.get('href')
+        date = article.find('span', class_='post__time').text
+        post_title = article.find('h2', class_='post__title').text.strip()
+        href = article.find('h2', class_='post__title').find('a').attrs.get('href')
+        text_preview = article.find('div', class_='post__text').text
+        if re.search(keywords_pattern, post_title, flags=re.I) or \
+                re.search(keywords_pattern, text_preview, flags=re.I) or \
+                hubs & keywords:
             result = f'Дата: {date}\nЗаголовок: {post_title}\nСсылка: {href}\n'
             article_info.append(result)
             print(result)
